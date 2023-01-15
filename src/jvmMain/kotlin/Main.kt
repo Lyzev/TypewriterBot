fb2c30bc77df9b02ab6b1a62364bebc9147b6ac0
lyzev <lyzev.info@gmail.com>
2023-01-15 13:40:14 +0100

    - Added documentation
    Signed-off-by: lyzev <lyzev.info@gmail.com>

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -20,9 +20,13 @@ import java.awt.Desktop
 import java.net.URI
 import java.util.*
 
+/**
+ * This class is a Composable function that creates a GUI for a TypewriterBot using the Compose library
+ */
 @Composable
 @Preview
 fun App() {
+    // Open a URL in the browser
     fun openInBrowser(uri: URI) {
         val osName by lazy(LazyThreadSafetyMode.NONE) { System.getProperty("os.name").lowercase(Locale.getDefault()) }
         val desktop = Desktop.getDesktop()
@@ -56,7 +60,7 @@ fun App() {
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-
+                // Display the GitHub icon and user's GitHub
                 OutlinedButton({ openInBrowser(URI("https://github.com/Lyzev")) }) {
                     Row(
                         horizontalArrangement = Arrangement.Center, verticalAlignment = Alignment.CenterVertically
@@ -67,16 +71,20 @@ fun App() {
                     }
                 }
 
+                // Spacer for formatting
                 Spacer(Modifier.height(5.dp))
 
+                // Text field for username input
                 OutlinedTextField(value = username,
                     onValueChange = { username = it },
                     label = { Text("Username") },
                     singleLine = true,
                     placeholder = { Text("Username") })
 
+                // Spacer for formatting
                 Spacer(Modifier.height(5.dp))
 
+                // Text field for password input
                 OutlinedTextField(value = password,
                     onValueChange = { password = it },
                     label = { Text("Password") },
@@ -92,8 +100,10 @@ fun App() {
                         }
                     })
 
+                // Spacer for formatting
                 Spacer(Modifier.height(5.dp))
 
+                // Text field for typing speed input
                 OutlinedTextField(value = wpm.toString(),
                     onValueChange = { value ->
                         if (value.length in 1..3) wpm = value.filter { it.isDigit() }.toInt()
@@ -105,8 +115,10 @@ fun App() {
                     keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
                 )
 
+                // Spacer for formatting
                 Spacer(Modifier.height(5.dp))
 
+                // Text field for error probability input
                 OutlinedTextField(value = errorProbability.toString(),
                     onValueChange = { value ->
                         if (value.length in 1..3) errorProbability = value.filter { it.isDigit() }.toInt()
@@ -118,8 +130,10 @@ fun App() {
                     keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
                 )
 
+                // Spacer for formatting
                 Spacer(Modifier.height(5.dp))
 
+                // Text field for number of levels input
                 OutlinedTextField(value = levels.toString(),
                     onValueChange = { value ->
                         if (value.length in 1..3) levels = value.filter { it.isDigit() }.toInt()
@@ -131,27 +145,27 @@ fun App() {
                     keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
                 )
 
+                // Spacer for formatting
                 Spacer(Modifier.height(5.dp))
 
+                // Button to start/stop the bot
                 OutlinedButton({
                     if (typewriterBot != null && typewriterBot!!.isRunning) {
                         Thread {
                             enabled = false
-                            typewriterBot!!.stop()
+                            typewriterBot?.stop()
                             typewriterBot = null
                             enabled = true
                         }.start()
                     } else {
                         val delay = 1000.0 / ((wpm * 5.0) / 60.0)
-                        val minDelay = delay * .95
-                        val maxDelay = delay * 1.05
                         Thread {
                             enabled = false
                             typewriterBot = TypewriterBot(
-                                username, password, minDelay.toLong(), maxDelay.toLong(), errorProbability, levels
+                                username, password, (delay * .95).toLong(), (delay * 1.05).toLong(), errorProbability, levels
                             )
                             enabled = true
-                            typewriterBot!!.start()
+                            typewriterBot?.start()
                         }.start()
                     }
                 }, enabled = enabled) {
@@ -163,7 +177,10 @@ fun App() {
 }
 
 fun main() = application {
+    // Sets the path to the driver
     System.setProperty("webdriver.gecko.driver", "geckodriver.exe")
+
+    // Creates a window
     Window(
         onCloseRequest = ::exitApplication, state = rememberWindowState(
             WindowPlacement.Floating, false, WindowPosition(Alignment.Center), 350.dp, 500.dp
--- src/jvmMain/kotlin/TypewriterBot.kt
+++ src/jvmMain/kotlin/TypewriterBot.kt
@@ -5,28 +5,69 @@ import org.openqa.selenium.support.ui.WebDriverWait
 import java.security.SecureRandom
 import java.time.Duration
 
-
+/**
+ * This class is a bot that automates the process of typing on the website "at4.typewriter.at"
+ * It uses the Selenium library to interact with the website and complete levels.
+ * It simulates human-like typing by introducing errors with a specified probability and using random delays between keystrokes.
+ */
 class TypewriterBot(
+    /**
+     * The username to use when logging in to the website
+     */
     private val username: String,
+    /**
+     * The password to use when logging in to the website
+     */
     private val password: String,
+    /**
+     * The minimum delay, in milliseconds, between keystrokes
+     */
     private val minDelay: Long,
+    /**
+     * The maximum delay, in milliseconds, between keystrokes
+     */
     private val maxDelay: Long,
+    /**
+     * The probability, as a percentage, of introducing errors while typing
+     */
     private val errorProbability: Int,
+    /**
+     * The number of levels to complete
+     */
     private val levels: Int
 ) {
 
+    /**
+     * Property that indicates if the bot is currently running
+     */
     var isRunning = false
         private set
 
+    /**
+     * The FirefoxDriver instance used to interact with the website
+     */
     private val driver = FirefoxDriver()
+
+    /**
+     * The WebDriverWait instance used to wait for elements to load on the website
+     */
     private val wait = WebDriverWait(driver, Duration.ofSeconds(10))
+
+    /**
+     * The SecureRandom instance used to generate random numbers for delays and errors
+     */
     private val random = SecureRandom.getInstanceStrong()
 
+    /**
+     * Start the bot
+     */
     fun start() {
         isRunning = true
         try {
+            // navigate to the website
             driver["https://at4.typewriter.at/"]
 
+            // locate and fill in the login form
             wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div[3]/div[2]/div[1]/form/div[1]/input")))
                 .sendKeys(username)
             wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div[3]/div[2]/div[1]/form/div[2]/input")))
@@ -34,11 +75,13 @@ class TypewriterBot(
             wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div[3]/div[2]/div[1]/form/div[3]/input")))
                 .click()
 
+            // wait for the user overview page to load
             wait.until(ExpectedConditions.urlToBe("https://at4.typewriter.at/index.php?r=user/overview"))
 
+            // complete the specified number of levels
             for (i in 1..levels) {
                 Thread.sleep(500)
-                if (!isRunning) break
+                if (!isRunning) break // check if bot was stopped
                 doLevel()
             }
         } catch (e: Exception) {
@@ -46,26 +89,47 @@ class TypewriterBot(
         }
     }
 
+    /**
+     * Stop the bot
+     */
     fun stop() {
         isRunning = false
         try {
+            // close the browser window
             driver.close()
         } catch (e: Exception) {
             e.printStackTrace()
         }
     }
 
+    /**
+     * Completes a single level
+     */
     private fun doLevel() {
+        // navigate to the level page
         driver["https://at4.typewriter.at/index.php?r=typewriter/runLevel"]
+
+        // click the start button
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div[6]/div[3]/div/button"))).click()
-        if (!isRunning) return
+
+        if (!isRunning) return // check if bot was stopped
+
         while (isRunning) {
+            // the current letter to type
             val actualLetter = driver.findElement(By.xpath("/html/body/div[3]/div[2]/div[3]/div[2]/div[2]/span[1]"))
+
+            // check if bot was stopped or if there are no more letters to type
             if (!isRunning || actualLetter == null || actualLetter.text == null || actualLetter.text.isEmpty()) return
-            if (random.nextInt(100) + 1 <= errorProbability)
+
+            if (random.nextInt(100) + 1 <= errorProbability) {
+                // type a random letter with errorProbability chance
                 driver.findElement(By.tagName("body")).sendKeys(((random.nextInt(26) + 'a'.code).toChar()).toString())
-            else
+            } else {
+                // type the correct letter
                 driver.findElement(By.tagName("body")).sendKeys(actualLetter.text)
+            }
+
+            // wait for a random delay between minDelay and maxDelay
             Thread.sleep(minDelay + random.nextInt((maxDelay - minDelay).toInt()))
         }
     }
